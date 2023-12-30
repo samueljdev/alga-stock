bb637316f60daf833a8d0b02b9d933cacb8696e5
samue <samuel.jdev11@gmail.com>
2023-12-29 23:48:22 -0400

    projeto

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
+
 import logo from './logo.svg';
 import './App.css';
+import TestCompoment from '../TestComponent/TestComponent';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+       
+       <TestCompoment />
+       
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/components/TestComponent/TestComponent.tsx
@@ -0,0 +1,12 @@
+
+import React from "react";
+import '../TestComponent/TestCompoment.css'
+
+// declaração de componente
+const TestCompoment = () => (
+    <div className="TestCompoment" > Teste componente </div>
+)
+
+
+export default TestCompoment
+//className é uma palavra reservada para criar classes dentro do JSX
