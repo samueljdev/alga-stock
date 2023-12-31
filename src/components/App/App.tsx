22efce71e41966bf961838489faf015a46c91c6a
samue <samuel.jdev11@gmail.com>
2023-12-31 00:40:42 -0400

    criando componente table

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,27 +1,23 @@
 
 import Button from '../../shared/Button';
 import Container from '../../shared/Container';
+import Input from '../../shared/Input';
 import Header from '../Header';
 import './App.css';
 
-function TestComponent() {
-  return <img width="16" src="https://img.icons8.com/pastel-glyph/2x/search--v2.png" alt="search icon" />
-}
 
 function App() {
+
   return (
     <div className="App">
       <Header title="AlgaStock" />
 
       <Container>
-        <Button
-          onClick={() => window.alert('UIIIU')}
-          appendIcon={<TestComponent />}
-        >
-          Alert
-        </Button>
+       <>
+       
+       </>
       </Container>
-      
+
     </div>
   );
 }
--- src/shared/Container/Container.tsx
+++ src/shared/Container/Container.tsx
@@ -1,10 +1,12 @@
 import React from "react"
 import './Container.css'
 
-const Container: React.FC<{ children: JSX.Element | JSX.Element[]}> = (props) => {
-    return <div className="AppContainer">
-      { props.children }
+const Container: React.FC<{ children: JSX.Element | JSX.Element[] }> = (props) => {
+  return (
+    <div className="AppContainer">
+      {props.children}
     </div>
-  }
-  
-  export default Container
+  )
+}
+
+export default Container
